16310dcb4597558727f889da8a82c49f6f4d213c
BrettGodmer <90002169+BrettGodmer@users.noreply.github.com>
2022-09-29 15:30:13 -0400

    fixing whitespace error

--- sailorsDB-create/db.sql
+++ sailorsDB-create/db.sql
@@ -9,11 +9,8 @@ SELECT * FROM Boats;
 SELECT * FROM Reservations;
 SELECT * FROM LazySailors;
 SET AUTOCOMMIT ON;
-INSERT INTO Sailors	
-	VALUES (22, 'Dave', 6, 45.0, 95);
-INSERT INTO Sailors
-	VALUES (21, 'Jay', 6, 45.0, 99);
-INSERT INTO Sailors
-	VALUES (92, 'Popeye', 17, 45.0, 95);
+INSERT INTO Sailors	VALUES (22, 'Dave', 6, 45.0, 95);
+INSERT INTO Sailors VALUES (21, 'Jay', 6, 45.0, 99);
+INSERT INTO Sailors VALUES (92, 'Popeye', 17, 45.0, 95);
 SET ECHO OFF
 SPOOL OFF
